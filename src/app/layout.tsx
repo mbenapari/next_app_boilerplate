b1e88570c00e15f689ca387f8c8422b42b6e3e5b
mbenapari <mbenapari@gmail.com>
2024-02-20 22:56:21 +0000

    project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+export const size = { width: 32, height: 32 }
+
+export const metadata: Metadata = {
+  title: "| Research GH",
+  description: "Ghana's Research Hub",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="./favicon.ico" sizes="any" />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
